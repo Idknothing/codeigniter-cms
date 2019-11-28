87d6d6dde2c71c76b8b90853face5258788ddc14
IdkNothing <edipididi@gmail.com>
2019-11-28 22:58:11 +0300

    Toggle button eklendi. Yeni ekle butonu eklendi. Kayıt yoktur Alert Paneli Eklendi.

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,9 +1,15 @@
 <div class="row">
   <div class="col-md-12">
-    <h4 class="m-b-lg">Ürün Listesi</h4>
+    <h4 class="m-b-lg">
+      Ürün Listesi
+      <a class="btn btn-primary btn-outline btn-xs pull-right" href="#"><i class="fa fa-plus"></i>Yeni Ekle</a>
+    </h4>
   </div><!-- END column -->
   <div class="col-md-12">
     <div class="widget p-lg">
+      <div class="alert alert-info alert-dismissible text-center">
+      	<p>Burada herhangi bir veri bulunamadı. Eklemek için lütfen <a href"">tıklayınız</a></p>
+      </div>
       <table class="table table-hover table-striped">
         <thead>
           <th>#id</th>
@@ -19,7 +25,9 @@
             <td>monitor-askisi</td>
             <td>Monitor Askısı</td>
             <td>360 derece kullanılabilen monitör askısıdır</td>
-            <td>1/0</td>
+            <td>
+              <input type="checkbox" data-switchery data-color="#10c469" checked />
+            </td>
             <td>
               <a href="" type="button" class="btn btn-outline btn-danger btn-sm"><i class="fa fa-trash"></i>Sil</a>
               <a href="" type="button" class="btn btn-outline btn-info btn-sm"><i class="fa fa-pencil-square"></i>Düzenle</a>
