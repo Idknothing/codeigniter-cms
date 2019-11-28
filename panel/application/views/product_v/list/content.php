c1a725abddfc6da253b6d87c15a61e23a54128a4
IdkNothing <edipididi@gmail.com>
2019-11-28 22:39:25 +0300

    Ürünler modülünün ürün listesi için html şablonu oluşturuldu

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,32 @@
-urun listesi content...
+<div class="row">
+  <div class="col-md-12">
+    <h4 class="m-b-lg">Ürün Listesi</h4>
+  </div><!-- END column -->
+  <div class="col-md-12">
+    <div class="widget p-lg">
+      <table class="table table-hover table-striped">
+        <thead>
+          <th>#id</th>
+          <th>url</th>
+          <th>Başlık</th>
+          <th>Açıklama</th>
+          <th>Durumu</th>
+          <th>İşlem</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>#1</td>
+            <td>monitor-askisi</td>
+            <td>Monitor Askısı</td>
+            <td>360 derece kullanılabilen monitör askısıdır</td>
+            <td>1/0</td>
+            <td>
+              <a href="" type="button" class="btn btn-outline btn-danger btn-sm"><i class="fa fa-trash"></i>Sil</a>
+              <a href="" type="button" class="btn btn-outline btn-info btn-sm"><i class="fa fa-pencil-square"></i>Düzenle</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div><!-- .widget -->
+  </div><!-- END column -->
+</div>
